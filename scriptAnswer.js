64639c7e7d454f86c102394d0e793583ece01123
naumov210 <naumov210@gmail.com>
2018-12-25 15:36:53 +0200

    second commit

--- scriptAnswer.js
+++ scriptAnswer.js
@@ -1,35 +1,3 @@
-;
-'use strict';
-
-var money = +prompt("Ваш бюджет на месяц ?", "");
-var time = prompt("Введите дату в формате YYYY-MM-DD", "YYYY-MM-DD");
-
-var appData = {
-    money,
-    time,
-    expenses,
-    income,
-    savings: false
-}
-
-
-var a = prompt("Введите обязательную статью расходов в этом месяце", "");
-var b = +prompt("Во сколько обойдётся?", "");
-
-var expenses = {
-    a,
-    b
-}
-
-
-var optionalExpenses = {};
-
-var income = [];
-
-var budgetOneDay = money / 30;
-alert(budgetOneDay);
-
-
 let money = prompt("Ваш бюджет на месяц?", ''),
 	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
